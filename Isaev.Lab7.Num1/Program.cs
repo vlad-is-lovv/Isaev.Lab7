f0d51c7a8f09614ef5c26541fee6a10dbef67a6f
arinameliakina <melyakina2003@mail.ru>
2022-06-04 18:33:50 +0300

    Добавьте файлы проекта.

---
+++ Isaev.Lab7.Num1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+    #region "Var.1"
+    {
+        var str = Console.ReadLine().ToCharArray();
+        int countWords = 0;
+        int id = 0;
+        //Ставим флаг поиска пробела в false - это позволит 
+        //избежать ошибок, если в начале строки будут пробелы
+        bool findSpace = false;
+        //Идём по символам строки
+        while (id < str.Length)
+        {
+            //Если нашли пробел
+            if (str[id] == ' ')
+            {
+                //Проверяем - искали-ли мы его.
+                //Если искали, то ставим флаг того, что нашли, 
+                //если не искали (ищщем уже не пробел) - просто идём дальше.
+                if (findSpace)
+                    findSpace = false;
+            }
+            //Если нашли не пробел
+            else
+            {
+                //Если искали пробел, то ничего не делаем, 
+                //а просто идём дальше, в противном случае
+                //Говорим, что мы нашлю символ-который-не-пробел,
+                //и увеличиваем счётчик
+                if (!findSpace)
+                {
+                    //Говорим, что теперь ищщем снова пробел
+                    findSpace = true;
+                    //Увеличиваем счётчик слов
+                    countWords++;
+                }
+            }
+
+            id++;
+        }
+        Console.WriteLine(countWords);
+    }
+    #endregion
+
+    #region "Var.2"
+    {
+        var str = Console.ReadLine();
+        var str1 = str.Trim().Split(' ');
+        Console.WriteLine(str1.Length);
+    }
+    #endregion
+
+
---
+++ Isaev.Lab7.Num2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+{
+    #region "Var.1"
+    {
+        var A = Console.ReadLine().ToCharArray();
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                str1.Append(A[i]);
+            }
+            if (i % 2 == 1)
+            {
+                str2.Append(A[i]);
+            }
+        }
+        Console.WriteLine(str1);
+        Console.WriteLine(str2);
+    }
+    #endregion
+
+    #region "Var.2"
+    {
+        var str = Console.ReadLine();
+        var res1 = string.Concat(str.Where((c, i) => i % 2 != 0)); //i % 2 == 0 если нумерация начинается с 0
+        var res2 = string.Concat(str.Where((c, i) => i % 2 == 0));//i % 2 != 0 если нумерация начинается с 0
+        Console.WriteLine(res1);
+        Console.WriteLine(res2);
+    }
+    #endregion
+}
